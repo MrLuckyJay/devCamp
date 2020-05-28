2615b643d5d8389730dc58be7cac18fb5bc397ae
MrLuckyJay <nabieutitusjusu@gmail.com>
2020-05-28 16:07:44 +0000

    add dummy data

--- config/db.js
+++ config/db.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const colors = require("colors");
 
 const connectionDB = async () => {
+  const conn = await mongoose.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
 
-    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    });
-
-    console.log(`Mongo DB connection Sucessful connected to: ${conn.connection.host}`)
-}
-
+  console.log(
+    `Mongo DB connection Sucessful connected to: ${conn.connection.host}`.cyan
+      .underline.bold
+  );
+};
 
 module.exports = connectionDB;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 // const mongoose = require("mongoose");
-const connectionDB = require('./config/db')
+const connectionDB = require("./config/db");
 
 //import environment file
 dotenv.config({
@@ -21,8 +22,6 @@ const app = express();
 //
 // const logger = require('./middleware/logger')
 
-
-
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -39,15 +38,17 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 //Handle unhandled rejections
 
-process.on('unhandledRejection', (error, promise) => {
-  console.log(`Erro: ${error.message}`)
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`Erro: ${error.message}`.red.bold);
 
   //close server and exit process
 
-  process.close(() => process.exit(1))
-})
+  process.close(() => process.exit(1));
+});
